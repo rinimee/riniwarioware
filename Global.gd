441aecd90e065ad4097f25bf6357571f621a1879
rinimee <srinikapola@gmail.com>
2026-07-31 15:08:02 -0400

    Coded timers and player movement

---
+++ Global.gd
@@ -0,0 +1,12 @@
+extends Node
+var minigames_done = 0 #track how many minigames done
+var lives = 5 #track how many lives left, also effecting garlic appearing
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
