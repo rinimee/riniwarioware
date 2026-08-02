f54e2047ea902633dcd9a548c5318d237604d3a2
rinimee <srinikapola@gmail.com>
2026-08-02 19:02:19 -0400

    Tried fixing global script

--- Global.gd
+++ Global.gd
@@ -1,5 +1,5 @@
 extends Node
-var minigames_done = 0 #track how many minigames done
+var minigames_done = 1 #track how many minigames done
 var lives = 5 #track how many lives left, also effecting garlic appearing
 
 # Called when the node enters the scene tree for the first time.
